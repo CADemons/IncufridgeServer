762b36abaaf9b2ded8454e21aee9ae87d16b04a1
LincB <LincB@users.noreply.github.com>
2014-07-17 11:51:49 -0400

    Navigation bar

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<div id="navbar">
+    <span class="navbar-item">Current User: <?php echo "placeholder"; ?></span>
+    <span class="navbar-item"><a class="topbar-link" href="home.php">Home</a></span>
+    <span class="navbar-item"><a class="topbar-link" href="prefs.php">Preferences</a></span>
+    <span class="navbar-item"><a class="topbar-link" href="logout.php">Logout</a></span>
+</div>
